8864a72ce69f4926df2837090a45dc112eeb5642
Juan Pablo Quiroga <jquiroga1991@gmail.com>
2018-06-26 22:04:09 -0400

    get four ventas terminada

--- kayaguiop.py
+++ kayaguiop.py
@@ -47,10 +47,10 @@ def next():
     writer = pd.ExcelWriter(week+'.xlsx')
 
     for i in range(len(arreglo_tiendas)):
-        saleMatrix=GetVentas(week,vt,arreglo_tiendas[i], vtSheet)
+        saleMatrix=GetFourVentas(vt, week, arreglo_tiendas[i], vtSheet)
         stockAMatrix=GetStock(stockA, arreglo_tiendas[i])
         stockBMatrix=GetStock(stockB, arreglo_tiendas[i])
-        rotaDataFrame=calcularRotation(saleMatrix, stockAMatrix, stockBMatrix)
+        rotaDataFrame=calcularRotation_B(saleMatrix, stockAMatrix, stockBMatrix, week)
         print("sheet tienda "+ arreglo_tiendas[i]+' CREADO CON EXITO')
         rotaDataFrame.to_excel(writer, arreglo_tiendas[i], header=False, index=False)
 
@@ -58,6 +58,65 @@ def next():
 
     print('Arhivo generado con exito')
 #Definicion funciones para trabajo de excels.
+def GetFourVentas(file, week, tienda, sheetName):
+    xl=pd.ExcelFile(file)
+    Ventas= (xl.parse(sheetName))
+    w , h = 6 , len(Ventas)
+    global Data
+    Data= [[ '' for x in range(w)] for y in range(h)]
+    global ventas
+    ventas= [[ '' for x in range(w)] for y in range(h)]
+    
+    headers = Ventas.dtypes.index
+    for i in range(len(Ventas.iloc[0])):
+        if (headers[i]=='Código'):
+            colCodProd=i
+        elif(headers[i]=='Producto'):
+            colProd=i
+        elif(headers[i]=='Cant. Facturada'):
+            colFact=i
+        elif(headers[i]=='SEMANA'):
+            colSemana=i
+        elif(headers[i]=='LOCAL'):
+            colTienda=i
+    i=0        
+    setSku=set()
+    for index, rows in Ventas.iterrows():
+        
+        if ( rows[colTienda]== tienda and(rows[colSemana] == week or rows[colSemana] == week-1 or rows[colSemana] == week -2 or rows[colSemana]== week-3 )):
+            ventas[i][0]=rows[colCodProd]
+            ventas[i][1]=rows[colProd]
+            ventas[i][2]=rows[colFact]
+            ventas[i][3]=rows[colSemana]
+            ventas[i][4]=rows[colTienda]
+            i=i+1
+            setSku.add(rows[colCodProd])
+    del(ventas[i:len(ventas)])
+    #Set de todos los sku sin repetir para rellenar el arreglo con ventas x sku x tienda semanas t, t-1, t-2, t-3
+    i=0
+    for sku in setSku:
+        print(sku)
+        Data[i][0]=sku     #sku
+      #  Data[i][1]=rows[colProd]     #Producto
+        for k in range(len(ventas)):
+            if(ventas[k][0]==sku): 
+                Data[i][1]=ventas[k][1]
+                if(ventas[k][3]==week):
+                    Data[i][5]=ventas[k][2]
+                elif(ventas[k][3]==week-1):
+                    Data[i][4]=ventas[k][2]
+                elif(ventas[k][3]==week-2):
+                    Data[i][3]=ventas[k][2]
+                elif(ventas[k][3]==week-3):
+                    Data[i][2]=ventas[k][2]
+        print(Data[i])
+        i=i+1
+    del(Data[i:len(Data)])
+    print (pd.DataFrame(Data))
+    print("******Matriz de ventas tienda "+ tienda +" cargada con exito**********")
+    #print(Matriz)
+    return Data
+
 
 def GetVentas ( semana_B, file , tienda, sheetName):
     xl=pd.ExcelFile(file)
@@ -79,10 +138,6 @@ def GetVentas ( semana_B, file , tienda, sheetName):
         elif(headers[i]=='LOCAL'):
             colTienda=i
     i=0
-    """ Codigo para iterrows
-    for index, rows in ventas.iterrows():
-    print(rows['Bodega'])
-    """
     for index, rows in Ventas.iterrows():
         if(rows[colFact]!= -1  and rows[colSemana] == int(semana_B) and tienda == rows[colTienda]) :
             Data[i][0]=rows[colCodProd]     #sku
@@ -140,7 +195,52 @@ def GetStock(file, tienda):
     print("******Matriz de Stock "+tienda +" cargada con exito   *********")
     #print(pd.DataFrame(Data))
     return Data
-def calcularRotation( ventas, stock_a, stock_b):
+def calcularRotation_B( ventas, stock_a, stock_b, week):
+    Data= [[ '' for x in range(9)] for y in range(len(ventas))]
+    #Data=[]
+    len_a=len(stock_a)
+    len_b=len(stock_b)
+    Data[0][0]='SKU'
+    Data[0][1]='Nombre Producto'
+    Data[0][2]='Vendidos '+week-3
+    Data[0][3]='Vendidos '+week-2
+    Data[0][4]='Vendidos '+week-1
+    Data[0][5]='Vendidos '+week
+    Data[0][6]='Inventario Periodo Anterior'
+    Data[0][7]='Inventario Periodo'
+    Data[0][8]='Rotacion Periodo'
+    #Data[0][9]='Tienda'
+    Data[0][9]='Pedido Actual'
+    for i in range(1,len(ventas)):
+        Data[i][0]= ventas[i][0]     #SKU
+        Data[i][1]= ventas[i][1]     #Nombre
+        Data[i][2]= ventas[i][2]     #venta w-3
+        Data[i][3]= ventas[i][3]     #venta w-2
+        Data[i][4]= ventas[i][4]     #venta w-1 
+        Data[i][5]= ventas[i][5]     #venta w
+        for j in range(len_a):
+
+            if(ventas[i][0] == stock_a[j][0]):    #SKU iguales
+                    Data[i][6]=stock_a[j][2]  #inventario anterior
+                    break
+        for k in range(len_b):
+                if( ventas[i][0] == stock_b[k][0]):   #SKU iguales
+                    Data[i][7]=stock_b[k][2] #inventario actual
+                    break
+        if(type(Data[i][2])==str or  type(Data[i][3]) == str or  type(Data[i][4])==str):
+            Data[i][8]='No Hay info'
+
+        else:
+            #Data[i][9]= Data[i][2]/((Data[i][3] + Data[i][4])//2)
+            Data[i][9]="ingresar formula excel culiao"
+            Data[i][8]= math.ceil(Data[i][2]**Data[i][5])
+    print('*************DATA FRAME DE ROTACION DE INVENTARIO  ***************')
+    dataframe=pd.DataFrame(Data)
+    print(dataframe)
+    return dataframe
+
+
+def calcularRotation_A( ventas, stock_a, stock_b):
     Data= [[ '' for x in range(9)] for y in range(len(ventas))]
     #Data=[]
     len_a=len(stock_a)
@@ -155,7 +255,6 @@ def calcularRotation( ventas, stock_a, stock_b):
     Data[0][7]='Tienda'
     Data[0][8]='Pedido Actual'
     for i in range(1,len(ventas)):
-        contada=0
         Data[i][0]= ventas[i][0]     #SKU
         Data[i][1]= ventas[i][1]     #Nombre
         Data[i][2]= ventas[i][2]     #venta
